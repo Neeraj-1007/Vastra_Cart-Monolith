863f196cfdf305c80b40f8b8e7cb5f1499a2fa27
Neeraj-1007 <neerajkumarjha10@gmail.com>
2023-07-28 04:10:28 +0530

    Second commit

--- src/main/java/com/vastracart/controller/UserController.java
+++ src/main/java/com/vastracart/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.vastracart.controller;
 
 
+import com.vastracart.service.serviceInt.UserServiceInt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/Login")
 public class UserController {
+
+    @Autowired
+    UserServiceInt userServiceInt;
 }
